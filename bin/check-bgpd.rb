ab22e0fe7273370a78b01a758177814afc4d0916
Mikal Villa <mikalv@mikalv.net>
2018-11-28 19:35:03 +0100

    Adding OpenBGPd check

---
+++ bin/check-bgpd.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+#
+#   check-bgpd
+#
+# DESCRIPTION:
+#
+# OUTPUT:
+#   plain text
+#
+# PLATFORMS:
+#   OpenBSD
+#
+# DEPENDENCIES:
+#   gem: sensu-plugin
+
+require 'sensu-plugin/check/cli'
+
+class CheckOpenBGPd < Sensu::Plugin::Check::CLI
+
+  option :warn,
+         short: '-w WARN',
+         description: 'Percetage of peers in a connecting/not-working state before warning',
+         proc: proc(&:to_f),
+         default: 50
+
+  option :crit,
+         short: '-c CRIT',
+         description: 'Percetage of peers in a connecting/not-working state before critical',
+         proc: proc(&:to_f),
+         default: 90
+
+  def run
+    begin
+      num_peers = `bgpctl show | egrep -v "Connect$|Active$" | tail -n +2 | wc -l`.strip.to_i
+      num_issue_peers = `bgpctl show | egrep '(Active|Connect)$' | wc -l`.strip.to_i
+      num_fib_routes = `bgpctl show fib | tail -n +6 | wc -l`.strip.to_i
+      num_rib_routes = `bgpctl show rib | tail -n +7 | wc -l`.strip.to_i
+
+      total_peers = num_peers + num_issue_peers
+      peers_success_ratio = ((num_peers.to_f / total_peers.to_f) * 100).truncate(2)
+
+      message = "OpenBGPd running with #{num_peers+num_issue_peers} peers, #{peers_success_ratio}% (#{num_issue_peers}) in a connecting or errored state, #{num_fib_routes}. Knows FIB routes and #{num_rib_routes} RIB routes"
+      critical message if num_peers.zero?
+      ok message
+    rescue StandardError => e
+      unknown 'OpenBGPd command Failed: #{e}'
+    end
+  end
+
+end
